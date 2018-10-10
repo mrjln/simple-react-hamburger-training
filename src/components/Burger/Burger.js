15eded36a4fccf6d2d2c4e159afa56945d34a54f
Marjolein Tromp <marjolein.tromp@incentro.com>
2018-10-10 13:55:34 +0200

    map ingredient state to ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,23 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngrient/BurgerIngredient'
 
 const Burger = (props) => {
+    let transIngredients = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])]
+                .map((_, i) => {
+                    return <BurgerIngredient type={ingredientKey} key={ingredientKey + i}/>
+                })
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    if(transIngredients.length === 0) {
+        transIngredients = <p> Yo dawg, please add some ingredients on your burger </p>
+    }
     return (
       <div className={classes.Burger}>
           <BurgerIngredient type="bread-top"/>
-          <BurgerIngredient type="cheese"/>
-          <BurgerIngredient type="meat"/>
+          {transIngredients}
           <BurgerIngredient type="bread-bottom"/>
       </div>
     );

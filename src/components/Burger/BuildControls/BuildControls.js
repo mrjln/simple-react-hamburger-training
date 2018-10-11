4a82210443580172ebe5e701d14bed5ae86ee760
Marjolein Tromp <marjolein.tromp@incentro.com>
2018-10-11 16:00:24 +0200

    add current price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,6 +12,7 @@ const controls = [
 const BuildControls = (props) => {
     return (
         <div className={classes.BuildControls}>
+            <p>Current Price: {props.price.toFixed(2)}</p>
             {controls.map(ctrl => {
                 return <BuildControl
                     key={ctrl.label}

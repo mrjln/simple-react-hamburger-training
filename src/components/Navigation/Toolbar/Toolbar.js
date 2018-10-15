1234d7e9dfbcda0258439dfe6b968d2ca02332ba
Marjolein Tromp <marjolein.tromp@incentro.com>
2018-10-15 12:12:54 +0200

    Burger logo and navigation folders

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './Toolbar.module.css'
+import Logo from '../../Logo/Logo'
+
+const toolbar = (props) => {
+    return (
+        <header className={classes.Toolbar}>
+            <div>MENU</div>
+            <Logo/>
+            <nav>
+                <ul>
+                    ....
+                </ul>
+            </nav>
+
+        </header>
+    );
+}
+
+
+export default toolbar
